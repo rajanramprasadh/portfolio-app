d1fcafab798fafc4a8cf196bbebe26584bd0008a
ramprasadhv <ramprasadh.varadharajan@nirman.io>
2022-06-21 09:09:38 +0530

    Optimized responsiveness of the elements.

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -10,9 +10,9 @@ function Hero() {
     <div
       className={`w-full h-[800px] mx-auto transition-all duration-500 bg-[#F1F1F1] dark:bg-gray-900`}
     >
-      <div className="flex flex-col p-20 justify-venter items-center">
+      <div className="flex flex-col xl:p-20 justify-venter items-center">
         <div>
-          <h1 className="text-5xl md:text-9xl text-logo-blue font-bold py-20 text-center md:text-left">
+          <h1 className="text-5xl md:text-7xl xl:text-9xl text-logo-blue font-bold py-12 xl:py-20 text-center md:text-left">
             About Me.
           </h1>
         </div>
