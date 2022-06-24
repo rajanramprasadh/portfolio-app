c7134d73dbea2149992ab5e4471b28f92365b491
ramprasadhv <ramprasadh.varadharajan@nirman.io>
2022-06-24 15:16:00 +0530

    Added few changes to profile card and footer.

--- components/hero/profile-card/profile-card.tsx
+++ components/hero/profile-card/profile-card.tsx
@@ -8,15 +8,20 @@ interface PortFolioCardsProps {
 
 function ProfileCard({ imgSrc }: PortFolioCardsProps) {
   return (
-    <div className="flex flex-col-reverse xl:flex-row justify-center items-center py-10 w-100 h-2/4 md:w-1/2 md:h-2/4 bg-white rounded-md xl:divide-x-2 xl:divide-slate-800 xl:dark:divide-slate-100 shadow-md dark:bg-slate-800 transition-colors duration-500">
-      <div className="text-black dark:text-white px-10 divide-y-2 divide-gray-400 transition-colors duration-500">
+    <div className="flex flex-col-reverse xl:flex-row justify-center items-center py-10 w-100 h-2/4 md:w-1/2 md:h-2/4 bg-white rounded-md shadow-md xl:divide-x-2 xl:divide-slate-800 xl:dark:divide-slate-100 dark:bg-slate-800 transition-colors duration-500">
+      <div className="text-black dark:text-white px-10 transition-colors duration-500">
         <h1 className="text-3xl xl:text-5xl text-logo-blue font-bold pb-4 text-center xl:text-right">
           Ram Prasadh Varadharajan
         </h1>
-        <p className="text-2xl text-center xl:text-right">
-          React Developer <br /> (Sr Frontend Developer)
+        <div className="transition-all duration-500 h-0.5 bg-gray-400"></div>
+        <p className="text-2xl text-center xl:text-right mt-2">
+          React Developer
         </p>
+        <div className="text-center xl:text-right">
+          <pre>(Sr. Frontend Developer)</pre>
+        </div>
       </div>
+      {/* <div className="transition-all duration-500 w-0.5 h-auto bg-slate-800 dark:bg-slate-100"></div> */}
       <div className="text-black px-10">
         <div className="w-40 xl:w-80 h-auto">
           <Image
--- components/layout/footer/footer.tsx
+++ components/layout/footer/footer.tsx
@@ -6,7 +6,7 @@ function Footer() {
     <footer className="transition-all duration-500 bg-[#F1F1F1] dark:bg-gray-900">
       <div className="max-w-6xl mx-auto px-4 py-10 md:py-20">
         <div className="transition-all duration-500 h-0.5 bg-white dark:bg-gray-700"></div>
-        <div className="flex flex-col space-y-4 md:space-y-0 md:flex-row justify-between md:items-center mt-8">
+        <div className="flex flex-col space-y-4 md:space-y-0 xl:flex-row justify-center xl:justify-between items-center text-center mt-8">
           <div className="whitespace-normal">
             <p> &#169; Ram Prasadh Varadharajan. All Rights Reserved.</p>
           </div>
